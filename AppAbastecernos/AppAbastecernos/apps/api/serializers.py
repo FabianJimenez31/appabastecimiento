4341c161b7eedd89a55f2c446548c539a2b79ad4
Jairo Andres <andresbbx@gmail.com>
2020-03-24 22:53:15 -0500

    agrego serializacion para productos

--- AppAbastecernos/AppAbastecernos/apps/api/serializers.py
+++ AppAbastecernos/AppAbastecernos/apps/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import store
+from .models import store, product
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = product
+        fields = ['id','name', 'description']
+
 
 class StoreSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
     class Meta:
         model = store
-        fields = ['id','name','latitude','longitude']
+        fields = ['id','name','latitude','longitude','products']
 
 
